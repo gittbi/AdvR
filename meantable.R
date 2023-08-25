fe7d79e797fa3b8ddb3118ec0ae379ab8a1ccfac
Gitte Stokvad Brix <gitte@MacBook-Pro-tilhrende-Gitte.local>
2023-08-25 09:44:52 +0200

    change to meantable code: added "return" line

--- meantable.R
+++ meantable.R
@@ -1,20 +1,20 @@
-#' meantable()
+#' Title: meantable()
 #'
 #' @param x 
 #' @param colvar 
 #' @param rowvar 
 #' @param statvar 
 #'
-#' @return
+#' @return 
 #' @export
 #'
-#' @examples
+#' @examples 
 meantable <- function(x, colvar, rowvar, statvar) {
   df <- x %>% 
     group_by({{colvar}}, {{rowvar}}) %>% 
     summarise(avg_statvar = mean({{statvar}}, na.rm = TRUE), .groups = "drop")
   # df
-  reshape2::dcast(df, df[[1]] ~ df[[2]], value.var = "avg_statvar")
+  df2 <- reshape2::dcast(df, df[[1]] ~ df[[2]], value.var = "avg_statvar")
+  return = df2
 }
-
 
